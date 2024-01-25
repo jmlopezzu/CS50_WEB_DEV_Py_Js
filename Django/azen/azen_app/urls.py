8420dff705df5f1caf73cfe69ba0435fca0a6cc1
José Miguel López Zuluaga <38506793+jmlopezzu@users.noreply.github.com>
2024-01-24 22:19:13 -0500

    django

--- Django/azen/azen_app/urls.py
+++ Django/azen/azen_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
   path("", views.index, name="index"),
-  path("JoseM", views.JoseM, name="JoseM")
+  path("<str:name>", views.greet, name="greet"),
+  path("JoseM", views.JoseM, name="JoseM"),
+  path("azen", views.azen, name="azen")
 ]
 
--- Django/azen/azen_app/views.py
+++ Django/azen/azen_app/views.py
@@ -3,8 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-  return HttpResponse(" Hello, Azen in Django ")
+  return HttpResponse(" Hello, Azen in Django ! ")
 
-
+  
 def JoseM(request):
   return HttpResponse(" Hello JoseM ")
+
+def azen(request):
+  return HttpResponse(" Hello azen ")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, bienvenido ... {name.capitalize()}")
+
+
